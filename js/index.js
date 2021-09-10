f3aa3220b4c1c33b42fd4511984abe6df4e662ec
Rick Beyer <beyer.richard73@gmail.com>
2021-09-09 22:49:19 -0500

    Prevented multiple clicks of button

--- js/index.js
+++ js/index.js
@@ -2,19 +2,21 @@ const btnStart = document.getElementById('start');
 const mallet = document.querySelector('.mallet');
 const mole = document.querySelector('.mole');
 
-btnStart.addEventListener('click', function() {
+btnStart.addEventListener('click', function () {
     // add class to move mallet
-    mallet.classList.add('raise-mallet');
+    if (!mallet.classList.contains('raise-mallet')) {
+        mallet.classList.add('raise-mallet');
 
-    // setTimeout for 5 seconds
-    const malletTimer = setTimeout(() => {
-        // when timeout ends, remove class
-        mallet.classList.remove('raise-mallet');
-        mole.classList.remove('raise-mole');
-    }, 5000);
+        // setTimeout for 5 seconds
+        const malletTimer = setTimeout(() => {
+            // when timeout ends, remove class
+            mallet.classList.remove('raise-mallet');
+            mole.classList.remove('raise-mole');
+        }, 5000);
 
-    const moleTimer = setTimeout(() => {
-        mole.classList.add('raise-mole');
-    }, 2000);
-    
+        const moleTimer = setTimeout(() => {
+            mole.classList.add('raise-mole');
+        }, 2000);
+    }
+
 });
